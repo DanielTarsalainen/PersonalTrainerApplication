464fcdd360f861506df45474b3b9b4387fefeb4c
Daniel Tarsalainen <77921212+DanielTarsalainen@users.noreply.github.com>
2021-05-19 09:04:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,21 +42,3 @@ function App() {
 }
 
 export default App;
-
-{/* <Nav fill variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav> */}
-
